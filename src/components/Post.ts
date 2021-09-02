aafdeb63ef44478ecfb2ab98dee1d7b9df36ecf5
rachel-rebecca <Rarebecc@umich.edu>
2021-09-01 21:52:30 -0400

    cleaned up post.ts file

--- src/components/Post.ts
+++ src/components/Post.ts
@@ -17,23 +17,5 @@ export const postsArray: Post[] = [  {
   }
 ]
 
-// export interface PostProps {
-//   posts: Array<Post>;
-// }
-
-// export const postsArray = [
-//   {
-//     title: "React?",
-//     thought: "Not loving it"
-//   },
-//   {
-//     title: "Cats?",
-//     thought: "Still love em",
-//   },
-// ];
-
-// export const postsArr: PostProps[] = [
-//     {posts: posts}
-// ]
 
 export default Post;
